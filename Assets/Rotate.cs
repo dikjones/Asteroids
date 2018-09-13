7338ad73c90d237b635c16c4fa4bf999a27ab10f
Rich Jones <rich@rwjones.info>
2018-09-13 18:23:06 +0100

    First Commit
    First Commit

---
+++ Assets/Rotate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+	void Update () {
+
+   if (Input.GetKey(KeyCode.A))
+       transform.Rotate(Vector3.up * speed * Time.deltaTime);
+
+   if (Input.GetKey(KeyCode.D))
+       transform.Rotate(-Vector3.up * speed * Time.deltaTime);
+
+  }
+}
+
+//test
