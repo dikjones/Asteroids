7bbe5adfe89556cf276fad73d4401e249da7d3c3
Rich Jones <rich@rwjones.info>
2018-09-14 18:35:51 +0100

    Updated Spin Script

--- Assets/spin.cs
+++ Assets/spin.cs
@@ -1,19 +1,19 @@
 using UnityEngine;
 ﻿using System.Collections;
 
-public class spin : MonoBehaviour
+public class Spin : MonoBehaviour
 {
 
 	void Update ()
 	{
 		if (Input.GetKey(KeyCode.A))
 	 	{
-    		transform.Rotate(Vector3.up * Time.deltaTime, Space.World);
+    		transform.Rotate(Vector3.right * Time.deltaTime);
 		}
 
    if (Input.GetKey(KeyCode.D))
 	 {
-       transform.Rotate(-Vector3.up * Time.deltaTime);
+       transform.Rotate(-Vector3.right * Time.deltaTime);
 			 }
 
   	}
